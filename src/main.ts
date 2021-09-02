7c2a8e3dd0e0d1ecfb565729aa80a5b55a0f7ee9
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-09-01 22:59:41 -0400

    Done With Adventure Mode!

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-import "./style.css";
+import './style.css'
+// import React from 'react'
+
+fetch('https://sdg-octodex.herokuapp.com/')
+  .then((response) => {
+    return response.json()
+  })
+  .then((octocats) => {
+    console.log(octocats)
+
+    const newUl = document.querySelector('ul')
+
+    for (let i = 0; i < octocats.data.length; i++) {
+      const newLi = document.createElement('li')
+      newLi.textContent = octocats.data[i].name
+
+      newUl?.appendChild(newLi)
+
+      // newLi.document.appendChild(octocats.data[i])
+      // document.body.insertAfter(newUl, newLi)
+    }
+
+    // console.log(octocats.data[0].authors[0].image)
+  })
+
+// let author = document.getElementsByClassName('number')[0]
+
+// console.log(author)
